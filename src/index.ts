99581318c00c9fa756bc0290420cd8c00033bee9
Kibae Shin <kibae.shin@gmail.com>
2022-07-30 22:59:24 +0900

    Fix: indexing

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from './pii-metadata';
+export * from './pii-compliance-service';
+
+export * from './decorators/pii-id.decorator';
+export * from './decorators/pii-column.decorator';
+
+export * from './types/pii.type';
